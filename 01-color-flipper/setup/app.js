91930bdf762b10f348c5c09d6692cce9c7599256
unknown <wabrodor@gmail.com>
2021-05-26 18:43:18 +0300

    latest update

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("#btn");
+const text = document.querySelector('.color')
+
+btn.addEventListener("click",() =>{
+text.textContent = colors[getRandomNumber()];
+document.body.style.backgroundColor = colors[getRandomNumber()]
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("#btn");
+const hex = ["A", "B","C","D", "E", "F", "0","1", "2", "3","4","5","6","7", "9"]      
+const text = document.querySelector('.color')
+
+btn.addEventListener("click",() =>{
+    let  colors = "#";
+    for(let i = 0; i < 6; i++){
+     colors += hex[getRandomNumber()] ; 
+    }
+text.textContent = colors
+document.body.style.backgroundColor = colors
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
